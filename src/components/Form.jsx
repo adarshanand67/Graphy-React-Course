5601648bf44ad8ef9cd9379fab911f93f7914d3a
Adarsh Anand <adarsh.anand.20031@iitgoa.ac.in>
2023-02-17 11:44:35 +0530

    react hook form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,26 +7,62 @@ import {
   TextInput,
 } from "flowbite-react";
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 import { AiOutlineMail, AiTwotoneLock } from "react-icons/ai";
+
 const Form = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [comment, setComment] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [comment, setComment] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [comment, setComment] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [comment, setComment] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [comment, setComment] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [comment, setComment] = useState("");
+
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+
+  const onSubmit = (data) => console.log(data);
+
 
   // console.log(email, password);
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const data = {
-      email: email,
-      password: password,
-      comment: comment,
-    };
-    console.log(data);
-  };
+  // const handleSubmit = (e) => {
+  //   e.preventDefault();
+  //   const data = {
+  //     email: email,
+  //     password: password,
+  //     comment: comment,
+  //   };
+  //   console.log(data);
+  // };
+
   return (
     <div className="mx-auto max-w-lg">
       <form className="mx-auto flex flex-col gap-4">
         <div>
+          <div className="mb-2 block">
+            <Label htmlFor="firstName" value="Your First name" className="font-bold " />
+          </div>
+          <TextInput
+            {...register("firstName", { required: true })}
+            aria-invalid={errors.firstName ? "true" : "false"}
+          />
+          {errors.firstName?.type === "required" && (
+            <p role="alert">First name is required</p>
+          )}
+
           <div className="mb-2 block">
             <Label htmlFor="email1" value="Your email" className="font-bold " />
           </div>
@@ -38,6 +74,7 @@ const Form = () => {
             required={true}
             addon="@"
             onChange={(e) => setEmail(e.target.value)}
+            {...register("email", { required: true })}
           />
         </div>
         <div>
@@ -51,6 +88,7 @@ const Form = () => {
             onChange={(e) => setPassword(e.target.value)}
             icon={AiTwotoneLock}
             placeholder="********"
+            {...register("password", { required: true, maxLength: 20 })}
             helperText={
               <>
                 We'll never share your details. Read our{" "}
@@ -77,8 +115,14 @@ const Form = () => {
           required={true}
           rows={4}
           onChange={(e) => setComment(e.target.value)}
+          {...register("comment", { required: true })}
         />
-        <Button type="submit" onClick={handleSubmit}>
+        <select {...register("gender")}>
+          <option value="female">female</option>
+          <option value="male">male</option>
+          <option value="other">other</option>
+        </select>
+        <Button type="submit" onClick={handleSubmit(onSubmit)}>
           Submit
         </Button>
       </form>
